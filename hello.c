58c74e61a685bd55dbe66e32584b598f91b7c74d
xevansz <minithbmatthew@gmail.com>
2024-01-17 23:45:05 +0530

    hello world in message box using winAPI

---
+++ hello.c
@@ -0,0 +1,11 @@
+#include <windows.h>
+
+int main(void) {
+    MessageBoxW(
+            NULL,
+            L"My first message box!",
+            L"Hello World!",
+            MB_YESNOCANCEL | MB_ICONEXCLAMATION
+    );
+    return EXIT_SUCCESS;
+}
